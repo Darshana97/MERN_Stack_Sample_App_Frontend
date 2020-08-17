9b55d896b2e7797b420a5797771db4338d7aa060
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-17 23:19:02 +0530

    create todo

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,11 +1,11 @@
 import React , {Component} from 'react';
 import { render } from '@testing-library/react';
 
-export default class TodosList extends Component {
+export default class CreateTodo extends Component {
     render(){
         return (
             <div>
-                <p>Welcome to Tods List Component!</p>
+                <p>Welcome to Create Todos List Component!</p>
             </div>
         )
     }
