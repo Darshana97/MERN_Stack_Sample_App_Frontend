09dd08d5f8f74b00681667c3491c90ce880ab368
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-17 22:14:57 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
   return (
-
+    <Router>
     <div className='container'>
       <h2>MERN-Stack Todo App</h2>
     </div>
-    
+    </Router>
   );
 }
 
