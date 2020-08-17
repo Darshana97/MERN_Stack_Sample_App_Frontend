38dac585a3577335e2efaecaecd07a04aa40b4ed
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-17 23:24:44 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-function App() {
-  return (
-
-    <Router>
-    <div className='container'>
-      <h2>MERN-Stack Todo App</h2>
-    </div>
-    <Route path="/" exact component="{TodosList}" />
-    <Route path="/edit/:id" component="{EditTodo}" />
-    <Route path="/create" component="{CreateTodo}" />
-    </Router>
-
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+        <Route path="/" exact component="{TodosList}" />
+        <Route path="/edit/:id" component="{EditTodo}" />
+        <Route path="/create" component="{CreateTodo}" />
+      </Router>
+    );
+  }
 }
 
 export default App;
